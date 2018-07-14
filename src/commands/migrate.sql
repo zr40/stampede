bb03cd1c376f9ed89cffebcd925e5f3c3eee2054
Matthijs van der Vleuten <git@zr40.nl>
2018-07-15 00:13:02 +0200

    Remove vacuuming by stampede

--- src/commands/migrate.sql
+++ src/commands/migrate.sql
@@ -1,14 +1,2 @@
 \echo Applying migrations...
 select stampede.migrate();
-
-\echo
-\echo VACUUM ANALYZE...
-select stampede.clean_up();
-
-commit;
-vacuum analyze;
-begin;
-
-create or replace function stampede.clean_up() returns void as $$
-  drop function stampede.clean_up();
-$$ language sql;
