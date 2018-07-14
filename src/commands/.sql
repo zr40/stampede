26d5301314b0ecd167849946ee35e6e05555f2a4
Matthijs van der Vleuten <git@zr40.nl>
2018-07-14 23:09:16 +0200

    Add clone command

--- src/commands/.sql
+++ src/commands/.sql
@@ -12,5 +12,8 @@
 \echo 'drop      Clear the database and create the `public` schema'
 \echo 'reset     Clear the database, create the `public` schema, then apply all migrations'
 \echo
+\echo Commands for application development:
+\echo 'clone DB  Clear the database, clone the DB database, then apply all migrations'
+\echo
 \echo 'On production databases, destructive commands will warn and wait for 10 seconds'
 \echo 'before continuing. Hopefully, this will prevent disasters.'
