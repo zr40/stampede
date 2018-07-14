be31fbaf43be753cd76f2cfb2809ab229ea9168f
Matthijs van der Vleuten <git@zr40.nl>
2018-07-14 23:07:49 +0200

    Perform VACUUM ANALYZE after applying migrations

--- src/commands/reset.sql
+++ src/commands/reset.sql
@@ -1,7 +1,18 @@
-\echo 'Clearing database...'
+\echo Clearing database...
 select stampede.reset();
 
 \echo
-
 \echo Applying migrations...
 select stampede.migrate();
+
+\echo
+\echo VACUUM ANALYZE...
+select stampede.clean_up();
+
+commit;
+vacuum analyze;
+begin;
+
+create or replace function stampede.clean_up() returns void as $$
+  drop function stampede.clean_up();
+$$ language sql;
